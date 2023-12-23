e161b31508b957d5600a381d6b4f048cb9e0fdd0
beartoldyou <70007054+beartoldyou@users.noreply.github.com>
2023-12-23 10:52:52 -0300

    Update nav.php

--- app/views/nav.php
+++ app/views/nav.php
@@ -10,7 +10,7 @@
 <nav class="blue darken-2">
     <div class="nav-wrapper container">
         <a href="#" data-target="mobile-demo" class="sidenav-trigger right">
-            <img src="/MVC/config/materialize/img/menu-svgrepo-com.svg" alt="Menu Icon" style="style=wstyle="width: 10px; margin-right: 5px;"">
+            <img src="/MVC/config/materialize/img/menu-svgrepo-com.svg" alt="Menu Icon" style="width: 10px; margin-right: 5px;">
         </a>
         <a href="?router=Site/home/" class="brand-logo light">Cadastro Geral</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
